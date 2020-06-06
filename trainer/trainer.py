4b546eca3f107392dfc1dcb678d40baa3b7ff42c
Mike Sheldon <mike@mikeasoft.com>
2020-06-06 15:24:09 +0100

    Add copyright notice to trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# vim:set et sts=4 sw=4:
+#
+# Abalone Trainer - Fine tunes speech recognition models for individual users
+#
+# Copyright (c) 2020 Mike Sheldon <elleo@gnu.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gst", "1.0")
